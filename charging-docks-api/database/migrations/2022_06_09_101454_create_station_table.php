7002f4c992ebb7318bf7d7a03f6ccc5ebe98f636
vins-stha <shrs_samar@yahoo.com>
2022-06-09 13:58:48 +0300

    added models

---
+++ charging-docks-api/database/migrations/2022_06_09_101454_create_station_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //id, name, latitude, longitude, company_id, address
+    public function up()
+    {
+        Schema::create('station', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->unsignedBigInteger('company_id')->unique();
+
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('company')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('station');
+    }
+}
