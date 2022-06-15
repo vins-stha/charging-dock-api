0d1c95b2d62eb893715c23b94af04917ce1dc00e
vins-stha <shrs_samar@yahoo.com>
2022-06-16 00:51:42 +0300

    added more test for company api

--- charging-docks-api/tests/Feature/CompanyTest.php
+++ charging-docks-api/tests/Feature/CompanyTest.php
@@ -8,22 +8,12 @@ use Tests\TestCase;
 
 class CompanyTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/api/v1/company');
-
-        $response->assertStatus(200);
-    }
 
     public function testAddNewCompany()
     {
+
         $payload = [
-            'name' => 'ParentCompany1'
+            'name' => $this->faker->name
         ];
 
         $response = $this->json('post', '/api/v1/company', $payload);
@@ -38,6 +28,55 @@ class CompanyTest extends TestCase
                     'parent_company_id'
                 ]
             );
+
+    }
+
+    public function testGetAllCompanies()
+    {
+
+        $payloads = [
+            ['name' => $this->faker->name],
+            ['name' => $this->faker->name],
+            ['name' => $this->faker->name]
+        ];
+
+        foreach($payloads as $payload)
+        {
+            $response[] = json_decode($this->json('post', '/api/v1/company', $payload)->getContent());
+
+        }
+
+        $get_response = json_decode($this->json('get', '/api/v1/company')->getContent());
+
+        $this->assertEquals(count($get_response), count($payloads));
+
+    }
+
+
+    public function testGetCompanyById()
+    {
+
+        $payload = [
+            'name' => $this->faker->name,
+
+        ];
+
+        $post_response = json_decode($this->json('post', '/api/v1/company', $payload)->getContent());
+
+        $id = $post_response->id;
+
+        $get_response_json = $this->json('get','/api/v1/company/'.$id);
+        $get_response_json->assertJsonStructure([
+            'id',
+            'name',
+            'parent_company_id',
+            'created_at',
+            'updated_at',
+            'stations'
+        ]);
+
+        $get_response_obj = json_decode($get_response_json->getContent());
+        $this->assertEquals($get_response_obj->name, $payload['name']);
 
     }
 }
